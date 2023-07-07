8b710bff38ba7b826dee1eb0d38babe3b04cba37
Feelthecrit <138836890+Feelthecrit@users.noreply.github.com>
2023-07-07 04:12:12 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve login.html as the main page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+
+// Startuje server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
